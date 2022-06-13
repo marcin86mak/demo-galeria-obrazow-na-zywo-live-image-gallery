6a89ecae2a30265cb2ad48c0cfe2d7b613d234bf
X-plugin.pl <marcin86mak@gmail.com>
2022-06-13 20:21:01 +0200

    go-live

--- live-image-gallery/js/page.js
+++ live-image-gallery/js/page.js
@@ -1,18 +1,23 @@
 const chk = document.getElementById('chk');
 
-chk.addEventListener('change', () => {
-    document.body.classList.toggle('dark');
-});
+if(chk) {
+    chk.addEventListener('change', () => {
+        document.body.classList.toggle('dark');
+    });
+}
 
 // SOCIAL PANEL JS
 const floating_btn = document.querySelector('.floating-btn');
 const close_btn = document.querySelector('.close-btn');
 const social_panel_container = document.querySelector('.social-panel-container');
 
-floating_btn.addEventListener('click', () => {
-    social_panel_container.classList.toggle('visible')
-});
-
-close_btn.addEventListener('click', () => {
-    social_panel_container.classList.remove('visible')
-});
+if(floating_btn) {
+    floating_btn.addEventListener('click', () => {
+        social_panel_container.classList.toggle('visible')
+    });
+}
+if(close_btn) {
+    close_btn.addEventListener('click', () => {
+        social_panel_container.classList.remove('visible')
+    });
+}
