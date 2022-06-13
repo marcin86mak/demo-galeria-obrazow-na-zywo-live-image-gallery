0b57b975a8463dda941f02c1c7c979f6485e38c9
X-plugin.pl <marcin86mak@gmail.com>
2022-06-13 20:19:21 +0200

    go-live

--- live-image-gallery/js/plugin.js
+++ live-image-gallery/js/plugin.js
@@ -12,12 +12,12 @@ const popup = {
     },
     open: function($figure) {
         $('.gallery').addClass('pop');
-        $popup = $('<div class="popup" />').appendTo($('body'));
-        $fig = $figure.clone().appendTo($('.popup'));
-        $bg = $('<div class="bg" />').appendTo($('.popup'));
-        $close = $('<div class="close"><svg><use xlink:href="#close"></use></svg></div>').appendTo($fig);
-        $shadow = $('<div class="shadow" />').appendTo($fig);
-        src = $('img', $fig).attr('src');
+        const $popup = $('<div class="popup" />').appendTo($('body'));
+        const $fig = $figure.clone().appendTo($('.popup'));
+        const $bg = $('<div class="bg" />').appendTo($('.popup'));
+        const $close = $('<div class="close"><svg><use xlink:href="#close"></use></svg></div>').appendTo($fig);
+        const $shadow = $('<div class="shadow" />').appendTo($fig);
+        const src = $('img', $fig).attr('src');
         $shadow.css({backgroundImage: 'url(' + src + ')'});
         $bg.css({backgroundImage: 'url(' + src + ')'});
         setTimeout(function(){
